5a09427fc28ff4755d7d96fce9889e9b8bb2be59
Samuel Grigolato <samuel.grigolato@gmail.com>
2021-09-11 08:26:53 -0300

    Remove erro forçado

--- aula20210910/tarefas/tarefas/model.js
+++ aula20210910/tarefas/tarefas/model.js
@@ -7,7 +7,6 @@ const tarefas = [];
 
 export async function cadastrarTarefa (tarefa) {
   await pausar(25);
-  throw new Error('erro forçado');
   sequencial++;
   tarefas.push({
     id: sequencial,
