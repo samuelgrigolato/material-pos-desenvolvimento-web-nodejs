943b224670c584244f3a1cd6fe1267e9f17335a7
Samuel Grigolato <samuel.grigolato@gmail.com>
2023-10-21 10:18:20 -0300

    Resolve postgres host dynamically between the two supported run modes

--- base_frontend/knexfile.ts
+++ base_frontend/knexfile.ts
@@ -1,9 +1,10 @@
 import type { Knex } from 'knex';
+import runMode from './shared/run_mode';
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'postgresql',
-    connection: 'postgres://postgres:postgres@postgres:5432/postgres',
+    connection: `postgres://postgres:postgres@${runMode === 'dockerized' ? 'postgres' : '127.0.0.1'}:5432/postgres`,
     migrations: {
       tableName: 'knex_migrations',
       extension: 'ts',
--- base_frontend/shared/querybuilder.ts
+++ base_frontend/shared/querybuilder.ts
@@ -1,8 +1,9 @@
 import knexLib from 'knex';
+import runMode from './run_mode';
 
 const knex = knexLib({
   client: 'pg',
-  connection: 'postgres://postgres:postgres@postgres:5432/postgres',
+  connection: `postgres://postgres:postgres@${runMode == 'dockerized' ? 'postgres' : '127.0.0.1'}:5432/postgres`,
   debug: true
 });
 
